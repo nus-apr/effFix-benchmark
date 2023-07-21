d1c00845eb879adb678c28a05274af4d74c80c40
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2023-07-21 20:58:19 +0800

    add wget to deps

--- openssl-1/memory-leak-2/deps.sh
+++ openssl-1/memory-leak-2/deps.sh
@@ -1,6 +1,6 @@
 apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends  \
   gcc \
   make \
-  perl
-
+  perl \
+  wget
 
--- openssl-1/null-ptr-4/deps.sh
+++ openssl-1/null-ptr-4/deps.sh
@@ -1,6 +1,7 @@
 apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends  \
   gcc \
   make \
-  perl
+  perl \
+  wget
 
 
