159b9424174d382b06bef433448d239670ad5f40
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2023-08-22 11:59:11 +0800

    update build scripts to build specific module

--- openssl-1/memory-leak-4/build.sh
+++ openssl-1/memory-leak-4/build.sh
@@ -6,7 +6,7 @@ bug_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
 dir_name=$1/$benchmark_name/$project_name/$bug_id
 cd $dir_name/src
 
-make -j`nproc`
+make -j`nproc` build_engines
 ret=$?
 exit $ret
 
