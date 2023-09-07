d6d58175eb53048f77c6e05eceb0f9784deb94d5
Zhang Yuntong <ang.unong@gmail.com>
2023-09-07 19:56:17 +0800

    Add subject-level setup scripts

---
+++ linux-kernel-5/subject_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+dir_name=$1
+current_dir=$PWD
+mkdir -p $dir_name
+cd $dir_name
+
+project_url=https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/
+bug_commit_id=v5.0-rc8
+
+cd $dir_name
+git clone $project_url src
+cd src
+git checkout $bug_commit_id
---
+++ snort/subject_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+dir_name=$1
+current_dir=$PWD
+mkdir -p $dir_name
+cd $dir_name
+
+download_link=https://raw.githubusercontent.com/nus-apr/efffix-benchmark/main/archives/$project_name.tar.gz
+
+mkdir tmp
+wget $download_link
+tar -xzf $project_name.tar.gz -C tmp
+mv tmp/$project_name src
+rm -rf tmp
