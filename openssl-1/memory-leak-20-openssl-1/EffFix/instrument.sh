23d4b003e49e5d5d9632a9879f2fd952d054ad9a
Zhang Yuntong <ang.unong@gmail.com>
2024-03-12 11:37:59 +0800

    Move pulse_args from meta file to instrument.sh

---
+++ openssl-1/memory-leak-20-openssl-1/EffFix/instrument.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 4 | rev)
+project_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
+bug_id=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
+dir_name=/experiment/$benchmark_name/$project_name/$bug_id
+self_dir_name=$dir_name/EffFix
+mkdir $self_dir_name
+
+config_path=$self_dir_name/repair.conf
+
+cat > $config_path <<EOL
+pulse_args=--pulse-max-disjuncts 100 --pulse-model-malloc-pattern CRYPTO_malloc --pulse-model-free-pattern CRYPTO_free --pulse-model-realloc-pattern CRYPTO_realloc
+EOL
